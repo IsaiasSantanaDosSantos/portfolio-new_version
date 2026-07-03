b802a2e3d2ebcb80cbff4657f666d103e87fe8b2
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2026-07-02 21:30:53 -0300

    chore(paths): validate path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+import { TestAlias } from '@utils/test-alias.ts';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TestAlias />
     <App />
   </StrictMode>,
 );
