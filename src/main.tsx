3ec22992632e298fc7a9376778dcaaa73cf321e8
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2026-07-03 23:24:06 -0300

    chore(styles): configure theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
 import App from './App.tsx';
+import { GlobalStyle, theme } from '@styles';
 
 import { TestAlias } from '@utils/test-alias.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestAlias />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <TestAlias />
+
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
