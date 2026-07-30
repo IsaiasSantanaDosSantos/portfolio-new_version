6f368a489ba2150782cfbf49d33fff95059d2879
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2026-07-29 21:46:02 -0300

    ci: configure github actions

---
+++ src/test/setup.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Infrastructure', () => {
+  it('should configure the testing environment correctly', () => {
+    expect(true).toBe(true);
+  });
+});
