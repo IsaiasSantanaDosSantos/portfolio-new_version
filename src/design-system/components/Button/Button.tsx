c9b19904ee26c82fbf2180c32db46eeb7c8d118f
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2026-08-06 21:44:48 -0300

    refactor(components): consolidate duplicated primitives into

--- src/design-system/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-import * as S from './styles';
-
-import { ButtonProps } from './types';
-
-export function Button({ children, ...props }: ButtonProps) {
-  return <S.Container {...props}>{children}</S.Container>;
-}
--- src/design-system/components/Input/Input.tsx
+++
@@ -1,7 +0,0 @@
-import * as S from './styles';
-
-import { InputProps } from './types';
-
-export function Input(props: InputProps) {
-  return <S.Container {...props} />;
-}
